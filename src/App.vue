42d7aef2e10641e3668330ddc17cded48289849a
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-13 16:38:02 +0300

    dell: unused components

--- src/App.vue
+++ src/App.vue
@@ -1,32 +1,40 @@
 <template>
-  <div id="app">
-    <nav>
-      <router-link to="/">Home</router-link> |
-      <router-link to="/about">About</router-link>
-    </nav>
-    <router-view/>
+  <div class="wrapper">
+    <header>
+      <div class="navbar">
+        <div class="container">
+          <div class="navbar-content">
+            <div class="logo">Vue CLI</div>
+            <ul class="navbar-list">
+              <li class="navbar-item" v-for="link in links" :key="link.title">
+                <router-link class="navbar-link" :title="link.title" :to="link.url">{{link.title}}</router-link>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+    </header>
   </div>
 </template>
 
-<style lang="scss">
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-}
-
-nav {
-  padding: 30px;
-
-  a {
-    font-weight: bold;
-    color: #2c3e50;
-
-    &.router-link-exact-active {
-      color: #42b983;
+<script>
+export default {
+  data() {
+    return {
+      links: [
+        {title: 'Home', url: '/'},
+        {title: 'Shop', url: '/shop'}
+      ]
     }
+
+  }
+}
+</script>
+
+<style lang="scss">
+.navbar-link {
+  &.router-link-exact-active {
+    color: #5247e7;
   }
 }
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,8 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import HomeView from '../views/HomeView.vue'
+import Shop from '@/views/Shop'
+import NotFound from '@/views/404'
+import Home from "@/views/Home";
 
 Vue.use(VueRouter)
 
@@ -8,15 +10,17 @@ const routes = [
   {
     path: '/',
     name: 'home',
-    component: HomeView
+    component: Home
   },
   {
-    path: '/about',
-    name: 'about',
-    // route level code-splitting
-    // this generates a separate chunk (about.[hash].js) for this route
-    // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/AboutView.vue')
+    path: '*',
+    name: 'notFound',
+    component: NotFound
+  } ,
+  {
+    path: '/shop',
+    name: 'shop',
+    component: Shop
   }
 ]
 
