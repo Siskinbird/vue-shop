10079faf9d7038802aecb510971aba3c4be0627c
Dmitrii Chizhov <dimachig@gmail.com>
2022-12-14 07:40:18 +0300

    add: Items in to store

--- src/views/Shop.vue
+++ src/views/Shop.vue
@@ -2,18 +2,27 @@
   <div class="wrapper-content wrapper-content--fixed">
     <section>
       <div class="container">
-        <h1>Example Page</h1>
+        <h1>Shop Page</h1>
+        <div class="item__wrapper">
+        <ShopItem v-for="product in shopList" :key="product.id" :product="product"/>
+        </div>
       </div>
     </section>
   </div>
 </template>
 <script>
+import ShopItem from '../components/ShopItem'
 export default {
+
+  components: {
+    ShopItem
+  },
   data() {
     return {
       shopList: null
     }
   },
+
   created() {
     this.shopList = this.$store.getters.getShopList
     console.log(this.shopList);
